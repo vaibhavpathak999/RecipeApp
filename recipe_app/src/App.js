8e7a560846c2131e6537c25e1bdabdc1a56f9693
vaibhavpathak999 <vaibhavpathak999@gmail.com>
2021-10-18 20:16:06 +0530

    create files and folders

--- recipe_app/src/App.js
+++ recipe_app/src/App.js
@@ -1,10 +1,44 @@
 import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+//Components
+import Homepage from './pages/Homepage';
+import Navbar from './pages/layout/Navbar';
+
+import Routes from './pages/routing/Routes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+//Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { loadUser } from './redux/actions/auth';
+
+import setAuthToken from './helpers/setAuthToken';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
-    <div className="App">
-      <h1>This is the Recipe App</h1>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <>
+          <Navbar/>
+          <ToastContainer newestOnTop autoClose={2000} />
+
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route component={Routes} />
+          </Switch>
+        </>
+      </Router>
+    </Provider>
   );
 }
 
